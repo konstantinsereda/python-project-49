d3139586b3ef5536b5cba3ba38754b05038fef5d
Konstantin Sereda <konst.sereda@gmail.com>
2024-01-05 19:17:53 +0500

    add prompt for input name

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+import sys
+sys.path.insert(0, '/Users/konst/Hexlet/python-project-49/brain_games')
+from cli import welcome_user
+
 
 def main():
     print('Welcome to the Brain Games!')
+    print('Hello, ' + welcome_user() + '!')
 
 
 if __name__ == '__main__':
